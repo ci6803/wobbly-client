4d1a02e84de38171b44e1cc0e05ffed1886e6ea7
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-08 15:22:39 +0100

    login page done

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -9,67 +9,71 @@ const [email, setEmail] = useState("");
 const [username, setUsername] = useState("");
 const [password, setPassword] = useState("");
 const [name, setName] = useState("");
-const [errorMessage, setErrorMessage] = useState(null)
 
 const handleEmail = (e) => setEmail(e.target.value);
 const handleUsername = (e) => setUsername(e.target.value);
 const handlePassword = (e) => setPassword(e.target.value);
 const handleName = (e) => setName(e.target.value);
 
+const navigate = useNavigate();
 
-}
+const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password, name, username};
 
-export default function SignupPage() {
+    axios.post(`${API_URL}/auth/signup`, requestBody)
+    .then((response) => {
+        navigate('/login');
+    })
+    .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+    })
+};
+
   return (
     <div className="SignupPage">
         <h1>Please Sign up Here</h1>
 
-    <form>
+    <form onSubmit={handleSignupSubmit}>
         <label>Username</label>
         <input 
         type="text"
         name="username"
-        value={username} />
+        value={username} 
+        onChange={handleUsername}/>
 
         <label>Email</label>
         <input 
         type="email"
         name="email"
-        value={email} />
+        value={email}
+        onChange={handleEmail} />
 
         <label>Password</label>
         <input  
         type="password"
         name="password"
-        value={password}/>
+        value={password}
+        onChange={handlePassword}/>
 
         <label>Full Name</label>
         <input 
         type="text"
         name="name"
-        value={name} />
+        value={name} 
+        onChange={handleName}/>
 
         <hr>
         </hr>
 
     <button type="submit">Sign up here</button>
-
-    <h4>If you already have an account click here</h4>
     </form>
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <h4>If you already have an account click here</h4>
+    
 
     </div>
   )
 }
+
+export default SignUpPage;

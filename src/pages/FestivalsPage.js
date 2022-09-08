ab7c58c2f48bbdfb534257ba31261456000e6b12
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-08 13:24:48 +0200

    list festivals added

--- src/pages/FestivalsPage.js
+++ src/pages/FestivalsPage.js
@@ -1,8 +1,29 @@
-
 import React from 'react'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import FestivalCard from '../components/FestivalCard/FestivalCard';
+const API_URL = "http://localhost:5005";
 
 export default function FestivalsPage() {
+
+  const [festivals, setFestivals] = useState([]);
+
+  const getAllFestivals = () => {
+    axios
+      .get(`${API_URL}/api/festival`)
+      .then(response => setFestivals(response.data))
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    getAllFestivals();
+  }, [])
+
   return (
-    <div>FestivalsPage</div>
+    <div className="FestivalsPage">
+
+    {festivals.map((festival) => <FestivalCard key={festival._id} {...festival}/>)}
+
+    </div>
   )
 }

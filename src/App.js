b96d801ad7aa4304531d3e0b5f1eac029dcb6625
ci6803 <ci6803@gmail.com>
2022-09-08 12:39:06 +0200

    routes-front-end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-import './App.css';
+import HomePage from "./pages/HomePage";
+import FestivalPage from "./pages/FestivalsPage";
+import FestivalDetailsPage from "./pages/FestivalDetailsPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignupPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/festival" element={<FestivalPage />} />
+        <Route path="/festival/:festivalId" element={<FestivalDetailsPage />} />
+        <Route path="/log-in" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Routes>
     </div>
   );
 }

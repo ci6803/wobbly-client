ea94bcb6352bac1be3e631db08c730ae493be4a4
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-08 13:55:05 +0100

    intial form done

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -2,6 +2,55 @@ import React from 'react'
 
 export default function SignupPage() {
   return (
-    <div>SignupPage</div>
+    <div className="SignupPage">
+        <h1>Please Sign up Here</h1>
+
+    <form>
+        <label>Username</label>
+        <input 
+        type="text"
+        name="username"
+        value={username} />
+
+        <label>Email</label>
+        <input 
+        type="email"
+        name="email"
+        value={email} />
+
+        <label>Password</label>
+        <input  
+        type="password"
+        name="password"
+        value={password}/>
+
+        <label>Full Name</label>
+        <input 
+        type="text"
+        name="name"
+        value={name} />
+
+        <hr>
+        </hr>
+
+    <button type="submit">Sign up here</button>
+
+    <h4>If you already have an account click here</h4>
+    </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
   )
 }

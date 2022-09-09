4874e9acdea2a534cd9ce3aa00c2affa6383b529
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-09 14:21:24 +0100

    provider auth implemented

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -6,7 +6,7 @@ const API_URL = "http://localhost:5005";
 
 const AuthContext = React.createContext();
 
-function AuthProviderWrapper() {
+function AuthProviderWrapper(props) {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
@@ -16,14 +16,48 @@ function AuthProviderWrapper() {
         localStorage.setItem('authToken', token);
     }
 
-    const authenticateUser = () => {
-        
-    }
+     const authenticateUser = () => {
+        const storedToken = localStorage.getItem('authToken');
 
+        if(storedToken) {
+            axios.get(`${API_URL}/auth/verify`, {headers: {Authorization: `Bearer ${storedToken}`}})
+            .then((response) => {
+                const user = response.data;
+                setIsLoading(true);
+                setIsLoading(false);
+                setUser(user);
+            })
+            .catch((error) => {
+                setIsLoggedIn(false);
+                setIsLoading(false);
+                setUser(null);
+            });
+        } else {
+            setIsLoggedIn(false);
+            setIsLoading(false);
+            setUser(null);
+        }
+     }
+     const removeToken = () => {
+        localStorage.removeItem("authToken");
+     }
 
+     const logOutUser = () => {
+        removeToken();
+        authenticateUser();
+     }
 
+     useEffect(() => {
+        authenticateUser();
+     }, []);
+    return (
+        <AuthContext.Provider value={{storeToken, authenticateUser, logOutUser, isLoading, isLoggedIn, user}}>
 
+            {props.children}
+        </AuthContext.Provider>
 
-
+    )
 
 }
+
+export { AuthProviderWrapper, AuthContext};

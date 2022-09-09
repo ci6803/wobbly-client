79e68ab978e7749a51ded2b3f5d8cc0c0aafc38b
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-09 12:05:53 +0200

    festival details done

--- src/pages/FestivalDetailsPage.js
+++ src/pages/FestivalDetailsPage.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+const API_URL = "http://localhost:5005";
 
 export default function FestivalDetailsPage() {
+
+  const [festival, setFestival] = useState({});
+
+  const { festivalId } = useParams();
+
+  const getFestival = () => {
+    axios.get(`${API_URL}/api/festival/${festivalId}`)
+          .then((response) => {
+            const oneFestival = response.data;
+            console.log(oneFestival);
+            setFestival(oneFestival);
+          })
+          .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    console.log(festivalId);
+    getFestival();
+  }, [festivalId]);
+
   return (
-    <div>FestivalDetailsPage</div>
+    <div className='FestivalDetailsPage'>
+        <h1>{festival.name}</h1>
+        <img src={festival.image} alt='festivalImage'/> 
+        <p>{festival.description}</p>
+    </div>
   )
 }

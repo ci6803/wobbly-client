c3a111c5a0982858303c056a7b1f1ea2af09571d
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-12 15:38:22 +0200

    comments almost done

--- src/pages/FestivalDetailsPage.js
+++ src/pages/FestivalDetailsPage.js
@@ -1,18 +1,24 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 const API_URL = "http://localhost:5005";
 
 export default function FestivalDetailsPage() {
+  const { user } = useContext(AuthContext);
   const [festival, setFestival] = useState({});
+  const [comment, setComment] = useState('');
+  const storedToken = localStorage.getItem('authToken');
 
   const { festivalId } = useParams();
+
+  const navigate = useNavigate();
 
   const getFestival = () => {
     const storedToken = localStorage.getItem("authToken");
 
     axios
-      .get(`${API_URL}/api/festival/${festivalId}`, { headers: {Authorization: `Bearer ${storedToken}`}})
+      .get(`${API_URL}/api/festival/${festivalId}`, {headers: {Authorization: `Bearer ${storedToken}`}})
       .then((response) => {
         const oneFestival = response.data;
         console.log(oneFestival);
@@ -20,6 +26,22 @@ export default function FestivalDetailsPage() {
       })
       .catch((err) => console.log(err));
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    axios.post(`${API_URL}/api/festival/${festivalId}`, comment)
+    navigate('/festival');
+    setComment('');
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setComment({
+      message: value, 
+      user: user
+    })
+  }
+
 
   useEffect(() => {
     console.log(festivalId);
@@ -35,6 +57,13 @@ export default function FestivalDetailsPage() {
       <Link to={`/festival/edit/${festivalId}`}>
         <button>Edit Festival</button>
       </Link>
+
+      <h2>Add a comment:</h2>
+      <form onSubmit={handleSubmit}>
+          <label>Message:</label>
+          <input type='text' name='message' onChange={handleChange}/>
+          <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }

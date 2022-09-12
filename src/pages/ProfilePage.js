36333f63b1e0e566b8e4d591d159137949f4878d
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-12 16:56:20 +0100

    inital layout done

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,25 +1,130 @@
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { AuthContext } from '../context/auth.context';
-import { useContext } from 'react';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+ import { AuthContext } from '../context/auth.context';
+ import { useContext } from 'react';
 
+function Copyright() {
 
-export default function ProfilePage() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Wobbly
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3, 4, 5, 6];
+
+const theme = createTheme();
+
+export default function Album() {
   const { user } = useContext(AuthContext);
   return (
-    <div>
-
-      <h1>Welcome back {user.name}</h1>
-      <br>
-      </br>
-      <img src={user.image} alt="users-pic" width={300} />
-      <p>Followers</p>
-      <p>Rating</p>
-      <p>Message</p>
-      <Container>
-
-      <p>List of fav festivals</p>
-
-      </Container>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'orange',
+            pt: 4,
+            pb: 10,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h2"
+              variant="h"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Welcome {user.name}
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              
+            </Typography>
+            <Stack
+              sx={{ pt: 4 , bgcolor: 'blue'}}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Upload Image</Button>
+              <Button variant="outlined">See My Followers</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8, bgcolor: 'red'}} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' , bgcolor: 'green'}}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%', bgcolor: 'purple'
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 , bgcolor: 'pink'}}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'yellow', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          {/* Footer */}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="white"
+          component="p"
+        >
+          {/* Something here to give the footer a purpose! */}
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
 }

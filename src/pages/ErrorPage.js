ddabd37d26e33449bebcb144a891911251a8bbea
ci6803 <ci6803@gmail.com>
2022-09-15 17:12:53 +0200

    error page styling

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Alert from "@mui/material/Alert";
+import "../App.css";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="error-page">
+      <Alert variant="filled" severity="error" sx={{ mt: 10 }}>
+        This page does not exist — 404
+      </Alert>
+
+      <img
+        className="error-img"
+        src="https://media.giphy.com/media/l4pSWfjA3jkBLbXzy/giphy.gif"
+        alt="error"
+      />
+    </div>
+  );
 }
 
 export default ErrorPage;

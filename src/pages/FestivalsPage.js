1e61c5ccd6f02f0d0fbd3b9bb3d9ff8578d733dd
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-15 11:04:18 +0200

    changed button

--- src/pages/FestivalsPage.js
+++ src/pages/FestivalsPage.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import FestivalCard from '../components/FestivalCard/FestivalCard';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5005';
 
 export default function FestivalsPage() {
 
   const [festivals, setFestivals] = useState([]);
+
+  const navigate = useNavigate();
 
   const getAllFestivals = () => {
     axios
@@ -23,12 +27,17 @@ export default function FestivalsPage() {
   
   return (
     <div className='festival-styling'>
-      <div className="FestivalsPage">
-    
-    {festivals.map((festival) => <FestivalCard width="200" key={festival._id} {...festival}/>)}
-    </div>
-    <div>
-      <Link to='/festival/add'><h2>Create an Upcoming Festival</h2></Link>
+      <Button
+          sx={{color: 'black',bgcolor: 'white'}}
+          variant="contained"
+          onClick={() => {
+          navigate('/festival/add')
+        }}
+      >
+        Create Festival
+        </Button>
+    <div className="FestivalsPage">
+      {festivals.map((festival) => <FestivalCard width="200" key={festival._id} {...festival}/>)}
     </div>
     </div>
   )

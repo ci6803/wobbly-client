c4acb1076abb6ccee27ed5b3bca3a0af701214c5
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-15 08:51:50 +0100

    services fixed

--- src/api/service.js
+++ src/api/service.js
@@ -2,7 +2,7 @@ import axios from "axios";
  
 const api = axios.create({
 
-  baseURL: process.env.REACT_APP_API_URL || 'https://magenta-spider-cape.cyclic.app'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5005'
 
 });
  
